bfa49b03655b7f01d0204aa751768e75c68c43e7
Yuka Kato <satoimo.to.imo@gmail.com>
2018-02-23 00:05:45 +0900

    Now you can be refunded

--- vending_machine/buy_drinks.rb
+++ vending_machine/buy_drinks.rb
@@ -4,9 +4,13 @@ require_relative "./money.rb"
 vending_machine = VendingMachine.new
 
 while true do
-  puts "Insert money: "
+  puts "Insert money (enter 0 to refund): "
   amount = gets.chomp.to_i
+  break if amount.zero?
+
   vending_machine.input(Money.new(amount))
 
   puts "Total: #{vending_machine.summary}"
 end
+
+puts "Refund money: #{vending_machine.refund} Yen"
