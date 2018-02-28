0367972a65041da5cbaad1d207b860f977072fad
Yuka Kato <satoimo.to.imo@gmail.com>
2018-02-28 14:21:13 +0900

    "1回ずつ" は制約ではない（要件を読み違えていた）のでその実装を消した

--- vending_machine/vending_machine.rb
+++ vending_machine/vending_machine.rb
@@ -1,30 +1,12 @@
 class VendingMachine
-  MAX_INSERTION = {
-    10   => 1,
-    50   => 1,
-    100  => 1,
-    500  => 1,
-    1000 => 1
-  }.freeze
-
   attr_reader :summary
 
   def initialize
     @summary = 0
-    @insertion_count = {
-      10   => 0,
-      50   => 0,
-      100  => 0,
-      500  => 0,
-      1000 => 0
-    }
   end
 
   def input(money)
-    raise if reach_max_insertion?(money.amount)
-
     @summary = @summary + money.amount
-    @insertion_count[money.amount] += 1
   end
 
   def refund
@@ -32,11 +14,5 @@ class VendingMachine
     @summary = 0
 
     summary
-  end
-
-  private
-
-  def reach_max_insertion?(amount)
-    MAX_INSERTION[amount] == @insertion_count[amount]
   end
 end
