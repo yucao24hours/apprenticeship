2eb4caf8fdb3b457ea8420c935125f58f7cd24d1
Yuka Kato <satoimo.to.imo@gmail.com>
2018-03-01 14:01:09 +0900

    wip

--- vending_machine/vending_machine.rb
+++ vending_machine/vending_machine.rb
@@ -32,6 +32,10 @@ class VendingMachine
   end
 
   def can_buy?(drink)
-    @summary >= drink.price
+    # ここで @stocks にはハッシュ形式で個数を取得できたほうが便利そう
+    # {'cola' => 3, 'orange_juice' => 12}
+    # と思ったが、そうなると、Drink のオブジェクトは格納する必要がなくなる？？？？
+    # でも値段とかは保持してないとだめだと思うので,,,。
+    (@summary >= drink.price) && (@stocks[:cola] > 0)
   end
 end
