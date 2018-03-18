248c41605864d090104b5c96fd07e0507136f0f7
Yuka Kato <satoimo.to.imo@gmail.com>
2018-03-19 00:07:53 +0900

    CSV 導入

--- factory/managers/organism_manager.rb
+++ factory/managers/organism_manager.rb
@@ -1,7 +1,15 @@
+require "pry"
+require "csv"
+require_relative "../organism"
+
 class OrganismManager
+  CSV_FILE_PATH = "./csv/organism.csv".freeze
+
   class << self
     def process!
       puts "OrganismManager.process! called"
+        CSV.table(CSV_FILE_PATH, header_converters: nil) do |row|
+      end
     end
   end
 end
