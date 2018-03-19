74932ad6774caf75da0e4a13db93a6b0ef2c5be4
Yuka Kato <satoimo.to.imo@gmail.com>
2018-03-19 14:03:36 +0900

    wip

--- factory/managers/organism_manager.rb
+++ factory/managers/organism_manager.rb
@@ -8,7 +8,10 @@ class OrganismManager
   class << self
     def process!
       puts "OrganismManager.process! called"
-        CSV.table(CSV_FILE_PATH, header_converters: nil) do |row|
+
+      CSV.table(CSV_FILE_PATH, header_converters: nil) do |row|
+        puts row
+        hoge = Organism.new(name: row["name"], height: row["height"], age: row["age"])
       end
     end
   end
